41afbeaf3a38520416bbedecd2e7a395a899a47a
Santiago Bazet <163946085+santibazet@users.noreply.github.com>
2024-09-05 00:55:10 -0300

    Add files via upload

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ObligatorioPruebaRazor.Modelos;
+using ObligatorioWeb.Modelos;
+
+namespace ObligatorioWeb.Datos
+{
+    public class ApplicationDbContext : DbContext 
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Chofer> Chofer { get; set; }
+        public DbSet<Vehiculo> Vehiculo { get; set;}
+
+        internal async Task<Vehiculo> ObtenerVehiculoAntiguo()
+        {
+            return await Vehiculo.OrderBy(c => c.Anio).FirstOrDefaultAsync();
+        }
+    }
+}
