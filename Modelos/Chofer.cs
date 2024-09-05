39f6262d54127e8b08a78ebd0a98817a2bc10d9c
Santiago Bazet <163946085+santibazet@users.noreply.github.com>
2024-09-05 00:55:24 -0300

    Add files via upload

---
+++ Modelos/Chofer.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObligatorioWeb.Modelos
+{
+    public class Chofer
+    {
+        [Key]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Numero identificatorio")]
+        [RegularExpression(@"^\d{8}$", ErrorMessage = "El campo {0} debe tener exactamente 8 dígitos.")]
+        public string? Ci { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Nombre")]
+        public string? Nombre { get; set; }
+
+        [Display(Name = "Apellido")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string? Apellido { get; set; }
+
+        [Display(Name = "Edad")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Range(18, int.MaxValue, ErrorMessage = "El chofer debe tener al menos a 18 años.")]
+        public int? Edad { get; set; }
+
+        [Display(Name = "Vehiculo Asignado")]
+        public string? Vehiculo { get; set; } = "Sin asignar";
+    }
+}
---
+++ Modelos/Vehiculo.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObligatorioPruebaRazor.Modelos
+{
+    public class Vehiculo
+    {
+        [Key]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Numero identificatorio")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "El campo {0} debe tener exactamente 5 dígitos.")]
+        public string? CN { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Numero de matrícula")]
+        [RegularExpression(@"^\d{3} [A-Za-z]{4}$", ErrorMessage = "El campo {0} debe tener el formato correcto: 3 números seguidos de un espacio y 4 letras.")]
+        public string? Matricula { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Marca")]
+        public string? Marca { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Modelo")]
+        public string? Modelo { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Display(Name = "Año")]
+        [Range(1900, 2023, ErrorMessage = "El campo {0} debe estar entre 1900 y el año actual.")]
+        public int Anio { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Estado { get; set; } = "No asignado"; // Valor por defecto "No asignado"
+
+        [Display(Name = "Libre")]
+        public bool Libre { get; set; } = true;
+    }
+}
